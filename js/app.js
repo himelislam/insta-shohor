fb34d3603cc9e121175bc8525d1580529906d159
himelislam <himel7100@gmail.com>
2022-03-11 15:41:24 +0600

    repeating reported post on heart button bug fixed

--- js/app.js
+++ js/app.js
@@ -139,7 +139,9 @@ const showPosts = (posts) => {
     const productsContainer = document.getElementById( "posts" );
     productsContainer.innerHTML = "";
 
-    posts.forEach((post) => {
+    const remainingPosts = posts.filter(post => !reportedPostsId.includes(post.id)); 
+
+    remainingPosts.forEach((post) => {
         const div = createPost(post);
         productsContainer.appendChild(div);
     });
